b52d869ed8b129ff50b3a95daf5d5e5ec3089584
choihyojun1 <jihoon7245@naver.com>
2025-07-10 21:11:48 +0900

    음식 더 추가

--- frying_pan.gd
+++ frying_pan.gd
@@ -1,12 +1,11 @@
 extends Area2D
 
 var cooking_food: Node2D
-var cookable_food = ["meat"]
 
 func _on_body_entered(body: Node2D) -> void:
 	if cooking_food:
 		return
-	if body.is_in_group("food") and body.type in cookable_food and body.interactable:
+	if body.is_in_group("food") and body.grill_result and body.interactable:
 		cooking_food = body
 		body.global_position = global_position
 		body.interactable = false
